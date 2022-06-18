645bb7488a9abe053f769eaf1ecfe26ed1dd3f44
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-18 21:47:43 +0300

    Edit return

--- scrap/scrap.py
+++ scrap/scrap.py
@@ -8,14 +8,16 @@ def request(url):
 
 
 def get_articles(url, page):
+    list = []
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, 'html.parser')
         pages = soup.find_all('article', class_="tm-articles-list__item")
-        for item in pages:
+        for id, item in enumerate(pages):
             title = item.find(
-                'h2', class_='tm-article-snippet__title tm-article-snippet__title_h2').find('span')
+                'h2', class_='tm-article-snippet__title tm-article-snippet__title_h2').find('span').text
             link = item.find('h2', class_='tm-article-snippet__title tm-article-snippet__title_h2').find(
                 'a', class_='tm-article-snippet__title-link').get('href')
-            return title, link
+            list.append([id+1, title, f'https://habr.com{link}'])
+        return list
     else:
         return 'Page status code != 200'
