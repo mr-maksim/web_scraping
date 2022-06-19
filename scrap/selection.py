57977c33fa7fb5d89f9b32e97da69a8781bca0a7
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-19 14:06:30 +0300

    Added date display

--- scrap/selection.py
+++ scrap/selection.py
@@ -13,13 +13,17 @@ def get_pattern():
 def get_article_text(title, page):
     pattern = get_pattern()
     if page.status_code == 200:
+        soup = BeautifulSoup(page.content, 'html.parser')
         if len(re.findall(pattern, title.lower())) == 0:
-            soup = BeautifulSoup(page.content, 'html.parser')
+            date = soup.find(
+                'span', class_='tm-article-snippet__datetime-published').find('time')["title"].split(',')
             p = soup.find(
                 'div', xmlns="http://www.w3.org/1999/xhtml").text.lower()
             if len(re.findall(pattern[:-1], p)) == 0:
                 pass
             else:
-                print(f'{title} : {page.url}')
+                print(f'{date[0]} - {title} - {page.url}')
         else:
-            print(f'{title} : {page.url}')
+            date = soup.find(
+                'span', class_='tm-article-snippet__datetime-published').find('time')["title"].split(',')
+            print(f'{date[0]} - {title} - {page.url}')
