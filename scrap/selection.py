74c084de49ae3ddfa5c76f84b2e706d4728fe2aa
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-19 13:39:00 +0300

    Added function "get_pattern" and refactoring

--- scrap/selection.py
+++ scrap/selection.py
@@ -3,9 +3,23 @@ from config import KEYWORDS
 from bs4 import BeautifulSoup
 
 
+def get_pattern():
+    pattern = ''
+    for item in KEYWORDS:
+        pattern += item+'|'
+    return pattern[:-1]
+
+
 def get_article_text(title, page):
-    
+    pattern = get_pattern()
     if page.status_code == 200:
-        soup = BeautifulSoup(page.content, 'html.parser')
-        p = soup.find('div', class_="article-formatted-body article-formatted-body article-formatted-body_version-2").find(
-            'div', xmlns="http://www.w3.org/1999/xhtml").text
+        if len(re.findall(pattern, title.lower())) == 0:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            p = soup.find(
+                'div', xmlns="http://www.w3.org/1999/xhtml").text.lower()
+            if len(re.findall(pattern[:-1], p)) == 0:
+                pass
+            else:
+                print(f'{title} : {page.url}')
+        else:
+            print(f'{title} : {page.url}')
