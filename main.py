991c212fb6b4f89caf34f661b958df5003ce10bb
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-19 14:10:40 +0300

    Refactoring main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scrap.scrap import get_articles, request
-from scrap.selection import get_article_text
+from scrap.selection import get_article_text,get_pattern
 from config import BASE_URL
-from tqdm import tqdm
 
 if __name__ == '__main__':
     list = get_articles(BASE_URL, request(BASE_URL))
+    print(f'{("*")*10} Слова для поиска : {get_pattern()} {("*")*10}')
     for item in list:
         get_article_text(item[1], request(item[2]))
