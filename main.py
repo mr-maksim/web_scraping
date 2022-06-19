8a2515c333a3a67af6206488e7ebbbb5b0a4c62b
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-19 13:39:28 +0300

    Minor changes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from scrap.scrap import get_articles, request
 from scrap.selection import get_article_text
-from config import KEYWORDS, BASE_URL
+from config import BASE_URL
 from tqdm import tqdm
 
 if __name__ == '__main__':
     list = get_articles(BASE_URL, request(BASE_URL))
-    for item in tqdm(list,'Анализ'):
-        # print(item)
-        get_article_text(request(item[1],item[2]))
+    for item in list:
+        get_article_text(item[1], request(item[2]))
