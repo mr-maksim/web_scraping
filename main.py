e5269df1c46b0e8dde5e8095177813ea7a2f8630
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-19 12:14:38 +0300

    Added "if __name__"

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from scrap.scrap import get_articles, request
+from scrap.selection import get_article_text
 from config import KEYWORDS, BASE_URL
+from tqdm import tqdm
 
-
-print(get_articles(BASE_URL, request(BASE_URL)))
+if __name__ == '__main__':
+    list = get_articles(BASE_URL, request(BASE_URL))
+    for item in tqdm(list,'Анализ'):
+        # print(item)
+        get_article_text(request(item[1],item[2]))
